796728613d693eb5c3b8ea41971128117ce80545
nlam15 <130080697+nlam15@users.noreply.github.com>
2023-05-29 18:47:50 -0700

    lab14

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,1 +1,22 @@
+// index.js - Working with your partner, experiment with JS events.
+// Author: Nathan Lam
+// Date: May 15, 2023
 
+//name sorting function
+function fixName(userName) {
+    var nameArray = userName.split('');
+    console.log("Your array name is: " + nameArray);
+    var nameArraySort = nameArray.sort((a, b) => a.localeCompare(b));
+    console.log("Your sorted array name is: " + nameArraySort);
+    var nameSorted = nameArraySort.join('');
+    console.log("Your sorted name is: ", nameSorted);
+    return nameSorted;
+  }
+  
+  var button = document.getElementById("my-button");
+  button.addEventListener("click", function() {
+    var inputValve = document.getElementById("user-name").value;
+    var newName = fixName(inputValue);
+    var output = document.getElementById("output");
+    output.innerHTML = "Here is your new name: " + newName;
+  });
--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -2,20 +2,23 @@
 // Author: Nathan Lam
 // Date: May 15, 2023
 
-function sortMe(str) {
-	return str.split('').sort().join('');
+//name sorting function
+function fixName(userName) {
+  var nameArray = userName.split('');
+  console.log("Your array name is: " + nameArray);
+  var nameArraySort = nameArray.sort((a, b) => a.localeCompare(b));
+  console.log("Your sorted array name is: " + nameArraySort);
+  var nameSorted = nameArraySort.join('');
+  console.log("Your sorted name is: ", nameSorted);
+  return nameSorted;
 }
 
-var buttonEl = document.getElementById("my-button");
-// event in the JS file that listens for a button click.
-buttonEl.addEventListener("click", function(){
-	console.log("Button pressed");
-  // prompts user to input their name with prompt() function. 
-  //var name = prompt("What is your name?");
-  var name = document.getElementById("getit").value;
-  var nameStr = "Hello, " + sortMe(name);
-  // changes text from "Hello, World!" to "Hello + user name"
-  var helloEl = document.getElementById("hello-world");
-  helloEl.innerHTML = nameStr;
-})
+var button = document.getElementById("my-button");
+button.addEventListener("click", function() {
+  var inputValue = document.getElementById("user-name").value;
+  var newName = fixName(inputValue);
+  var output = document.getElementById("output");
+  output.innerHTML = "Here is your new name: " + newName;
+});
+
  
